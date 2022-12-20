a62d8ae6ac061db2c8355fdf84213a51cb0c79e7
Norman McKeown <normanmckeown@duck.com>
2022-12-20 01:32:36 +0000

    move util module to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod config;
 pub mod games;
 pub mod error;
 pub mod profile;
+pub mod util;
 
 pub mod wine {
     use std::collections::HashMap;
@@ -24,51 +25,5 @@ pub mod wine {
                 env: Default::default(),
             }
         }
-    }
-}
-
-pub mod util {
-    use std::{fs, fs::File, iter::FromIterator, path::PathBuf};
-    use std::path::Path;
-    use crate::error::ToryggError;
-    use crate::games;
-
-    pub fn get_libraryfolders_vdf() -> PathBuf {
-        PathBuf::from(std::env::var("HOME").unwrap()).join(".steam/root/config/libraryfolders.vdf")
-    }
-
-    pub fn get_steam_library(app: &games::SteamApp) -> Result<PathBuf, ToryggError> {
-        let vdf = get_libraryfolders_vdf();
-        let mut file = File::open(vdf)?;
-        let kvs = torygg_vdf::parse(&mut file)?;
-
-        for kv in &kvs {
-            let components = kv.0.iter().collect::<Vec<_>>();
-            // Key we want:                    🠗
-            // libraryfolders/<lib_id>/apps/<appid>
-            if let Some(component) = components.get(3) {
-                if *component == app.appid.to_string().as_str() {
-                    // libraryfolders/<lib_id>/path
-                    let path = PathBuf::from_iter(kv.0.iter().take(2)).join("path");
-
-                    return Ok(kvs[&path].clone().into());
-                }
-            }
-        }
-
-        Err(ToryggError::SteamLibraryNotFound)
-    }
-
-    pub fn verify_directory(path: &Path) -> Result<(), ToryggError> {
-        if path.exists() {
-            return if !path.is_dir() {
-                Err(ToryggError::NotADirectory(path.to_owned()))
-            } else {
-                Ok(())
-            }
-        }
-
-        fs::create_dir(path)?;
-        Ok(())
     }
 }
