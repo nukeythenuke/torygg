9d9fd18fb90666bdbc47a5cf5c821558cd5aaa9e
Norman McKeown <normanmckeown@duck.com>
2023-11-16 17:22:44 +0000

    Update dependencies, use std::sync::OnceLock

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,12 @@
 use crate::util::verify_directory;
 use std::path::PathBuf;
-use once_cell::sync::OnceCell;
+use std::sync::OnceLock;
 use crate::games;
 
 static APP_NAME: &str = "torygg";
 
 pub fn config_dir() -> &'static PathBuf {
-    static CONFIG_DIR: OnceCell<PathBuf> = OnceCell::new();
+    static CONFIG_DIR: OnceLock<PathBuf> = OnceLock::new();
     CONFIG_DIR.get_or_init(|| {
         let  path = dirs::config_dir().expect("could not find location for config directory");
         let dir = path.join(APP_NAME);
@@ -16,7 +16,7 @@ pub fn config_dir() -> &'static PathBuf {
 }
 
 pub fn data_dir() -> &'static PathBuf {
-    static DATA_DIR: OnceCell<PathBuf> = OnceCell::new();
+    static DATA_DIR: OnceLock<PathBuf> = OnceLock::new();
     DATA_DIR.get_or_init(|| {
         let path = dirs::data_dir().expect("Could not find location for data directory");
         let dir = path.join(APP_NAME);
@@ -26,7 +26,7 @@ pub fn data_dir() -> &'static PathBuf {
 }
 
 pub fn mods_dir(game: &impl games::Game) -> &'static PathBuf {
-    static MODS_DIR: OnceCell<PathBuf> = OnceCell::new();
+    static MODS_DIR: OnceLock<PathBuf> = OnceLock::new();
     MODS_DIR.get_or_init(|| {
         let dir = data_dir().join(game.name()).join("mods");
         verify_directory(&dir).expect("Could not create mods directory");
